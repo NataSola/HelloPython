470b081fbe76f43aed2c958395685024c7e619a4
NataSola <nsol2009@yandex.ru>
2022-01-21 14:53:56 +0300

    Add IMPORTS THIS

--- Hi.py
+++ Hi.py
@@ -1,2 +1,1 @@
-print ('hello')
-print ('O_o')
+import this
--- Python_basis.py
+++ Python_basis.py
@@ -1,33 +1,39 @@
 # Строчные / прописные
 
-name = 'иван иваныч иванов'
-print (name)
+name = "иван иваныч иванов"
+print(name)
 
 # первые заглавные
-name = name.title()  
-print (name)
+name = name.title()
+print(name)
 
 # все заглавные
 name = name.upper()
-print (name)
+print(name)
 
 # все прописные
 # предпочительный вариант: перед сохранением данных, всё перевести в нижний регистр. Перед выводом использовать нужный регистр
 name = name.lower()
-print (name)
+print(name)
 
 # удаление пробелов в начале / конце строки
-name = ' Nata '
+name = " Nata "
 print(f"'{name}'")
 
-name = ' Nata ' 
-name = name.rstrip() # удаление пробела в конце
+name = " Nata "
+name = name.rstrip()  # удаление пробела в конце
 print(f"'{name}'")
 
-name = ' Nata '
-name = name.lstrip() # удаление пробела в начале
+name = " Nata "
+name = name.lstrip()  # удаление пробела в начале
 print(f"'{name}'")
 
-name = ' Nata '
-name = name.strip() # удаление пробела с обеих сторон
+name = " Nata "
+name = name.strip()  # удаление пробела с обеих сторон
 print(f"'{name}'")
+
+# обрезка знаков после запятой
+a = 0.2456
+b = 3
+c = round(a * b, 2)
+print(c)
